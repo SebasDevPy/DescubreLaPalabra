60bf68afc3a3dd0182d02f60c1a4e6a9f536d798
justin <119630600+Rias05@users.noreply.github.com>
2024-07-01 11:07:51 -0300

    Add files via upload

---
+++ parcial3.2/parcial_2/Package_funciones/archivo_json.py
@@ -0,0 +1,24 @@
+import json
+
+def leer_puntuaciones_json():
+    try:
+        with open('puntuaciones.json', 'r') as file:
+            puntuaciones = json.load(file)
+            puntuaciones_ordenadas = dict(sorted(puntuaciones.items(), key=lambda item: item[1], reverse=True))
+    except FileNotFoundError:
+        print("Archivo JSON de puntuaciones no encontrado. Creando uno nuevo...")
+        puntuaciones_ordenadas = {}
+    except Exception as e:
+        print(f"Error al leer puntuaciones desde JSON: {e}")
+        puntuaciones_ordenadas = {}
+    return puntuaciones_ordenadas
+
+def guardar_puntuaciones_json(puntuaciones):
+    try:
+        with open('puntuaciones.json', 'w') as file:
+            json.dump(puntuaciones, file, indent=4)
+        print("Puntuaciones guardadas correctamente en puntuaciones.json")
+    except Exception as e:
+        print(f"Error al guardar puntuaciones en JSON: {e}")
+
+
